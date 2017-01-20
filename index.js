b362a018609a1b7177b3c7fd4ecbc7b87bca36f4
Zorgatone <zorgatone@gmail.com>
2017-01-20 20:17:05 +0100

    XHTML5

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const title = "My page title";
 app.get("/", (req, res) => {
   fs.readFile("www/index.pug", null, (err, txt) => {
     if (err) {
+      res.set("Content-type", "text/plain");
       res.end("Error");
     } else {
+      res.set("Content-type", "application/xhtml+xml;charset=UTF-8");
       res.end(pug.render(txt, {
         title
       }));
