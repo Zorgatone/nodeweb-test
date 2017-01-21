b437031f161475b728332e12221615fcdf858b3d
Zorgatone <zorgatone@gmail.com>
2017-01-21 11:50:28 +0100

    Example OOP routing

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const express = require("express");
-const pug = require("pug");
-const fs = require("fs");
+//const pug = require("pug");
+//const fs = require("fs");
+//const MongoClient = require("mongodb").MongoClient;
+//const url = "mongodb://localhost:27017";
 
 const app = express();
 
-const title = "My page title";
+const Router = new require("./lib/router");
 
-app.get("/", (req, res) => {
-  fs.readFile("www/index.pug", null, (err, txt) => {
-    if (err) {
-      res.set("Content-type", "text/plain");
-      res.end("Error");
-    } else {
-      res.set("Content-type", "application/xhtml+xml;charset=UTF-8");
-      res.end(pug.render(txt, {
-        title
-      }));
-    }
-  });
-});
+const router = new Router(app);
+const pages = require("./lib/pages");
+
+for (let i = 0, len = pages.length; i < len; i += 1) {
+  const Page = pages[i];
+  router.addPage(new Page());
+}
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
